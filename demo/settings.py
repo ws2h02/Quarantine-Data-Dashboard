b0890e007a3c88083b7bc1f0f39d3c285d07c6dd
ws2h02 <cky035@gmail.com>
2022-03-14 17:20:29 +0800

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -9,18 +9,19 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+from environs import Env
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = Env()
+env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rpg^4p*$hn$-ngtnl*!ly^$btzkkq!i7t=7!n*ql3fmyu09@54'
+SECRET_KEY = env.str('QDD_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
